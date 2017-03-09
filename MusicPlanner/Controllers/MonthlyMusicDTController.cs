98ead49fa43e62fc3fa5f9a398cdb2c70d577bb8
Musikboy79 <musikboy0579@hotmail.com>
2017-03-09 16:51:44 -0500

    Added Datatables and building out new page for Monthly Music index and add, edit, and delete views in model popup.

---
+++ MusicPlanner/Controllers/MonthlyMusicDTController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicPlanner.Controllers
+{
+    public class MonthlyMusicDTController : Controller
+    {
+        // GET: MonthlyMusicDT
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
