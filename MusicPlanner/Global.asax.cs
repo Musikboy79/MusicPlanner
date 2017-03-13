d74b2dda1dbead201e902c2b991ad9e7d2d4194b
Musikboy79 <musikboy0579@hotmail.com>
2017-03-12 22:43:24 -0400

    Changed MMusic to Data Tables
    I changed the Monthly Music grid to use Data Tables.

--- MusicPlanner/Global.asax.cs
+++ MusicPlanner/Global.asax.cs
@@ -16,6 +16,7 @@ namespace MusicPlanner
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            System.Web.Helpers.AntiForgeryConfig.SuppressXFrameOptionsHeader = true;
         }
     }
 }
