6111986bbb4d06328a7e5acbf1b1202f8ddde13a
musikboy79 <musikboy79@gmail.com>
2017-04-21 16:06:09 -0400

    Beginning to add the MP3 file page.  I added the model's for the database.

---
+++ MusicPlanner/Models/MP3FileDetailsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicPlanner.Models
+{
+    public class MP3FileDetailsModel
+    {
+        public int Id { get; set; }
+        [Display(Name="Available Files")]
+        public string FileName { get; set; }
+        public byte[] FileContent { get; set; }
+
+    }
+}
---
+++ MusicPlanner/Models/MP3FileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicPlanner.Models
+{
+    public class MP3FileModel
+    {
+        [Required]
+        [DataType(DataType.Upload)]
+        [Display(Name="Select File")]
+        public HttpPostedFileBase mp3files { get; set; }
+    }
+}
