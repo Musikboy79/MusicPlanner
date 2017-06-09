25fc2b8c98ca7247b53791366a9fc287e721d929
Musikboy79 <musikboy0579@hotmail.com>
2017-06-08 20:18:58 -0400

    Adding page to upload and download Monthly Music Sheets

---
+++ MusicPlanner/Controllers/MonthlyMusicSheetController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security.Provider;
+using MusicPlanner.Models;
+
+namespace MusicPlanner.Controllers
+{
+    public class MonthlyMusicSheetController : Controller
+    {
+        // GET: MonthlyMusicSheet
+        public ActionResult MonthlyMusicSheetUpload()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult MusicSheetUpload(HttpPostedFileBase files)
+        {
+            String FileExt = Path.GetExtension(files.FileName).ToUpper();
+
+            if (FileExt == ".PDF")
+            {
+                Stream str = files.InputStream;
+                BinaryReader Br = new BinaryReader(str);
+                Byte[] FileDet = Br.ReadBytes((Int32) str.Length);
+
+                MonthlyMusicSheetDetailsModel Md = new Models.MonthlyMusicSheetDetailsModel();
+                Md.FileName = files.FileName;
+                Md.FileContent = FileDet;
+                SaveFileDetails(Md);
+                return RedirectToAction("MusicSheetUpload");
+            }
+
+            if (FileExt == ".DOC")
+            {
+                Stream str = files.InputStream;
+                BinaryReader Br = new BinaryReader(str);
+                Byte[] FileDet = Br.ReadBytes((Int32) str.Length);
+
+                MonthlyMusicSheetDetailsModel Md = new Models.MonthlyMusicSheetDetailsModel();
+                Md.FileName = files.FileName;
+                Md.FileContent = FileDet;
+                SaveFileDetails(Md);
+                return RedirectToAction("MusicSheetUpload");
+
+            }
+            else
+            {
+                ViewBag.FileStatus = "Invalid File Format";
+                return View();
+            }
+        }
+    }
+}
---
+++ MusicPlanner/Models/MonthlyMusicSheetDetailsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicPlanner.Models
+{
+    public class MonthlyMusicSheetDetailsModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Available Monthly Music Sheets")]
+        public string FileName { get; set; }
+        public byte[] FileContent { get; set; }
+
+    }
+}
