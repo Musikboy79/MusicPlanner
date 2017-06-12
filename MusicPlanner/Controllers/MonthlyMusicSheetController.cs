277fd7690abaaec4efb1ec991da70e9876ba9da7
Musikboy79 <musikboy0579@hotmail.com>
2017-06-12 13:21:00 -0400

    I finished the pages for Monthly Music Sheets, but it is throwing an exception that the file name field is null.

--- MusicPlanner/Controllers/MonthlyMusicSheetController.cs
+++ MusicPlanner/Controllers/MonthlyMusicSheetController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.Owin.Security.Provider;
 using MusicPlanner.Models;
+using System.IO;
+using Dapper;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
 
 namespace MusicPlanner.Controllers
 {
@@ -19,7 +23,7 @@ namespace MusicPlanner.Controllers
 
         [Authorize]
         [HttpPost]
-        public ActionResult MusicSheetUpload(HttpPostedFileBase files)
+        public ActionResult MonthlyMusicSheetUpload(HttpPostedFileBase files)
         {
             String FileExt = Path.GetExtension(files.FileName).ToUpper();
 
@@ -33,27 +37,67 @@ namespace MusicPlanner.Controllers
                 Md.FileName = files.FileName;
                 Md.FileContent = FileDet;
                 SaveFileDetails(Md);
-                return RedirectToAction("MusicSheetUpload");
-            }
-
-            if (FileExt == ".DOC")
-            {
-                Stream str = files.InputStream;
-                BinaryReader Br = new BinaryReader(str);
-                Byte[] FileDet = Br.ReadBytes((Int32) str.Length);
-
-                MonthlyMusicSheetDetailsModel Md = new Models.MonthlyMusicSheetDetailsModel();
-                Md.FileName = files.FileName;
-                Md.FileContent = FileDet;
-                SaveFileDetails(Md);
-                return RedirectToAction("MusicSheetUpload");
-
+                return RedirectToAction("MonthlyMusicSheetUpload");
             }
             else
             {
                 ViewBag.FileStatus = "Invalid File Format";
                 return View();
             }
+        }
+
+        [Authorize]
+        [HttpGet]
+        public FileResult DownloadFile(int id)
+        {
+            List<MonthlyMusicSheetDetailsModel> ObjFiles = GetFileList();
+
+            var FileById = (from FC in ObjFiles
+                    where FC.Id.Equals(id)
+                select new {FC.FileName, FC.FileContent}).ToList().FirstOrDefault();
+            return File(FileById.FileContent, "application/pdf", FileById.FileName);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public PartialViewResult MusicSheetFileDetails()
+        {
+            List<MonthlyMusicSheetDetailsModel> DetList = GetFileList();
+
+            return PartialView("MonthlyMusicSheetDetails", DetList);
+        }
+
+        [Authorize]
+        private List<MonthlyMusicSheetDetailsModel> GetFileList()
+        {
+            List<MonthlyMusicSheetDetailsModel> DetList = new List<MonthlyMusicSheetDetailsModel>();
+            DbConnection();
+            con.Open();
+            DetList = SqlMapper.Query<MonthlyMusicSheetDetailsModel>
+                (con, "GetMonthlyMusicSheetFileDetails", commandType:CommandType.StoredProcedure).ToList();
+            con.Close();
+            return DetList;
+        }
+
+        [Authorize]
+        private void SaveFileDetails(MonthlyMusicSheetDetailsModel objDet)
+        {
+            DynamicParameters Parm = new DynamicParameters();
+            Parm.Add("@FileName", objDet.FileName);
+            Parm.Add("@FileContent", objDet.FileContent);
+            DbConnection();
+            con.Open();
+            con.Execute("AddMonthlyMusicFileDetails", Parm, commandType: CommandType.StoredProcedure);
+            con.Close();
+        }
+
+        private SqlConnection con;
+        private string constr;
+
+        private void DbConnection()
+        {
+            constr = ConfigurationManager.ConnectionStrings["dbcon"].ToString();
+            con = new SqlConnection(constr);
         }
     }
 }
